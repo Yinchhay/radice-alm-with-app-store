c1f152aec4e7ff13600491b1e6b09fb2c33e5703
Choung Pheng Ear <45393928+Blizzard4E@users.noreply.github.com>
2024-03-09 20:01:57 +0700

    fix z-index bug on overlay

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -6,12 +6,12 @@ export default function Overlay({
     onClose?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }) {
     return (
-        <div className="fixed top-0 left-0 w-screen h-screen grid place-items-center">
+        <div className="fixed top-0 left-0 w-screen h-screen grid place-items-center z-10">
             <button
                 onClick={onClose}
                 className="fixed h-full w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20"
             ></button>
-            <div className="z-10">{children}</div>
+            <div className="z-20">{children}</div>
         </div>
     );
 }
