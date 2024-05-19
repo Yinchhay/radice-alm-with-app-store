e32852908af9ceccb244a82cf17399c7fa1ff75e
Choung Pheng Ear <45393928+Blizzard4E@users.noreply.github.com>
2024-05-19 17:04:30 +0700

    add categories to home page to test

--- src/app/_components/CategorySection.tsx
+++ src/app/_components/CategorySection.tsx
@@ -1,3 +1,12 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { FetchAssociatedProjectsData } from "../api/internal/project/associate/route";
+import {
+    getProjectsByCategory,
+    getProjectsByCategoryReturnType,
+} from "../fetch";
+
 export default function CategorySection({
     variant = "light",
     category,
@@ -12,13 +21,42 @@ export default function CategorySection({
         updatedAt: Date | null;
     };
 }) {
+    const [selectedProject, setSelectedProject] = useState<number>(0);
+    const [projects, setProjects] = useState<getProjectsByCategoryReturnType>(
+        [],
+    );
+
+    useEffect(() => {
+        async function loadProjects() {
+            const results = await getProjectsByCategory(category.id);
+            setProjects(results);
+            console.log(results);
+        }
+        loadProjects();
+    }, []);
     switch (variant) {
         case "light":
             return (
                 <div className="bg-white">
                     <div className="container mx-auto">
-                        {" "}
                         <h1 className="text-5xl font-bold">{category.name}</h1>
+                        {projects.length > 0 && (
+                            <div>
+                                <Image
+                                    src={
+                                        projects[selectedProject].logoUrl
+                                            ? (projects[selectedProject]
+                                                  .logoUrl as string)
+                                            : "/placeholder.webp"
+                                    }
+                                    width={80}
+                                    height={80}
+                                    alt=""
+                                />
+                                <h2>{projects[selectedProject].name}</h2>
+                                <p>{projects[selectedProject].description}</p>
+                            </div>
+                        )}
                     </div>
                 </div>
             );
--- src/app/fetch.ts
+++ src/app/fetch.ts
@@ -1,6 +1,6 @@
 "use server";
 import { db } from "@/drizzle/db";
-import { projectCategories, projects } from "@/drizzle/schema";
+import { categories, projectCategories, projects } from "@/drizzle/schema";
 import { eq, sql, or, inArray, count, and } from "drizzle-orm";
 
 export async function getCategories() {
@@ -8,4 +8,21 @@ export async function getCategories() {
     return categories;
 }
 
-export async function getProjectsByCategory(categoryId: number) {}
+export type getProjectsByCategoryReturnType = Awaited<
+    ReturnType<typeof getProjectsByCategory>
+>;
+export async function getProjectsByCategory(categoryId: number) {
+    return await db.query.projects.findMany({
+        with: {
+            projectCategories: {
+                with: {
+                    category: true,
+                },
+                where: (table, { eq }) => eq(table.categoryId, categoryId),
+            },
+            // projectMembers: true,
+            // projectPartners: true,
+        },
+        where: (table, { eq }) => eq(table.isPublic, false),
+    });
+}
