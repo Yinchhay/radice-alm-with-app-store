7a42323511a58f669bc8dd00d38f960497dd9db6
Choung Pheng Ear <45393928+Blizzard4E@users.noreply.github.com>
2024-07-07 23:44:30 +0700

    fix dark mode still affecting project status stepper

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -9,10 +9,42 @@ export default function Stepper({
     projectStatus: ProjectStatusElement[];
     isDashboard?: boolean;
 }) {
+    if (isDashboard) {
+        return (
+            <Card className="grid">
+                <h2 className="font-bold text-xl mb-4">Project Status</h2>
+                {projectStatus.map((status, i) => {
+                    return (
+                        <>
+                            <div className="flex gap-2">
+                                <div className="flex flex-col items-center">
+                                    {status.value ? (
+                                        <div className="p-1 rounded-full bg-blue-500 text-white">
+                                            <IconCheck />
+                                        </div>
+                                    ) : (
+                                        <div className="p-1 rounded-full bg-gray-200 text-gray-400">
+                                            <IconMinus stroke={2.5} />
+                                        </div>
+                                    )}
+                                    {i + 1 < projectStatus.length ? (
+                                        <div
+                                            className={`w-[3px] h-[20px] ${status.value || (i + 1 < projectStatus.length && projectStatus[i + 1].value) ? "bg-blue-500" : "bg-gray-400"}`}
+                                        ></div>
+                                    ) : (
+                                        ""
+                                    )}
+                                </div>
+                                <h4 className="mt-1">{status.name}</h4>
+                            </div>
+                        </>
+                    );
+                })}
+            </Card>
+        );
+    }
     return (
-        <Card
-            className={`grid ${isDashboard ? "" : "dark:bg-white dark:text-black dark:outline-gray-300"}`}
-        >
+        <div className="bg-white text-black rounded-sm outline outline-1 outline-gray-300 px-6 py-4">
             <h2 className="font-bold text-xl mb-4">Project Status</h2>
             {projectStatus.map((status, i) => {
                 return (
@@ -41,6 +73,6 @@ export default function Stepper({
                     </>
                 );
             })}
-        </Card>
+        </div>
     );
 }
