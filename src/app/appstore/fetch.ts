1392d348c96ac33740358801505fce9770435bfe
monn <pphan@paragoniu.edu.kh>
2025-06-28 03:32:33 +0700

    fetch data when user is login

---
+++ src/app/appstore/fetch.ts
@@ -0,0 +1,22 @@
+import { getBaseUrl, getSessionCookie } from "@/lib/server_utils";
+
+export async function fetchApps() {
+    try {
+        const sessionId = await getSessionCookie();
+        const response = await fetch(`${await getBaseUrl()}/api/internal/app`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${sessionId}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error("Fetch error:", error);
+        return { success: false, data: { apps: [] } };
+    }
+}
