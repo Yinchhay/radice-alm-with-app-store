0d1b29cd2c054af6e59979db1c5e0ee91f38e7e5
kolbotpen <kolbot.pen@gmail.com>
2025-07-01 11:55:34 +0700

    chore: update app routes and repository methods

--- src/repositories/app.ts
+++ src/repositories/app.ts
@@ -107,15 +107,6 @@ export const createApp = async (app: typeof apps.$inferInsert) => {
     return await db.insert(apps).values(app);
 };
 
-// Try to avoid using this function, use `getAppWithAllRelations` instead
-// export const getAppByProjectId = async (projectId: number) => {
-//     const result = await db
-//         .select()
-//         .from(apps)
-//         .where(eq(apps.projectId, projectId));
-//     return result;
-// };
-
 export async function getAppsForManageAllAppsTotalRow(search: string = "") {
     const totalRows = await db
         .select({ count: count() })
