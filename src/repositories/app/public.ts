73c59fcd010589f5599c80dcaed91044016be243
monn <pphan@paragoniu.edu.kh>
2025-07-04 00:30:07 +0700

    make public route retrives projectCategories

--- src/repositories/app/public.ts
+++ src/repositories/app/public.ts
@@ -53,8 +53,27 @@ export async function getAllAcceptedApps(){
     try {
         const acceptedApps = await db.query.apps.findMany({
             where: eq(apps.status, "accepted"),
+            with: {
+                project: {
+                    with: {
+                        projectCategories: {
+                            with: {
+                                category: true
+                            }
+                        }
+                    }
+                },
+                appType: true,
+                priority: true,
+                screenshots: true
+            }
         });
-        return acceptedApps;
+        return acceptedApps.map(app => ({
+            ...app,
+            category: app.project?.projectCategories?.[0]?.category || null,
+            projectCategories: app.project?.projectCategories?.[0] || null
+        }))
+        
     } catch (error) {
         console.error("Error fetching all accepted apps:", error);
         throw error;
