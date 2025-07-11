dbb62492888ffb13ee3db1ad5fd5758d5af25a57
monn <pphan@paragoniu.edu.kh>
2025-07-12 01:52:06 +0700

    making navbar and footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,8 +11,8 @@ const roboto_condensed = Roboto_Condensed({
 export default function Footer() {
     return (
         <footer className="bg-black text-white py-8">
-            <div className="container mx-auto grid grid-cols-4 gap-8">
-                <div className="col-span-2 max-w-[560px]">
+            <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="md:col-span-2 lg:col-span-2 max-w-[560px]">
                     <Link href={"/"} className="text-white text-lg font-bold">
                         <Image
                             src={"/RadiceLogo_dark.png"}
@@ -48,21 +48,21 @@ export default function Footer() {
                         <p>Copyright &copy; All Rights Reserved. 2024 RaDICe</p>
                     </div>
                 </div>
-                <div>
+                <div className="md:mt-0 mt-8">
                     <h2 className={`font-bold mb-4 text-xl`}>Contact Us</h2>
                     <p>
                         <span className="font-bold">Email</span>:
                         radi@paragoniu.edu.kh
                     </p>
                 </div>
-                <div>
+                <div className="md:mt-0 mt-8">
                     <h2 className={`font-bold mb-4 text-xl`}>Find Us</h2>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3908.5734929449427!2d104.89540667581117!3d11.582402688619641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109517bf7757d23%3A0x965c34888684bf1!2sParagon%20International%20University!5e0!3m2!1sen!2skh!4v1719135679821!5m2!1sen!2skh"
                         style={{ border: 0 }}
                         allowFullScreen={true}
                         loading="lazy"
-                        className="w-full h-[200px]"
+                        className="w-full h-[150px] sm:h-[200px]"
                         referrerPolicy="no-referrer-when-downgrade"
                     ></iframe>
                 </div>
