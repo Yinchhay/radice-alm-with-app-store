5c16caf50bdc5ead2391324320b2f6caf30aead9
sotheaseng <73590324+sotheaseng@users.noreply.github.com>
2025-07-18 14:45:31 +0700

    force dynamic link

--- src/app/api/public/app/route.ts
+++ src/app/api/public/app/route.ts
@@ -6,6 +6,7 @@ import {
 } from "@/lib/response";
 import { getAllAcceptedApps } from "@/repositories/app/public";
 
+export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
     try {
--- src/app/appstore/page.tsx
+++ src/app/appstore/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Metadata } from "next";
